9fccd1a18e0bb0e45263d59fb5111a7ea57533a8
Luis Padovani <luisfpadovani@outlook.com>
2019-04-03 19:38:35 -0300

    Alteração do projeto

--- src/main/java/br/com/fiap/orderservice/CustomException/ServerException.java
+++ src/main/java/br/com/fiap/orderservice/CustomException/ServerException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-public class ServerException extends RuntimeException {
+public class ServerException extends Exception {
     public ServerException(String message) {
         super(message);
     }
